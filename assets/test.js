bfb7c0a74f14431cf0bf7ab295b0b1fc740309f8
Anthony <an538811@gmail.com>
2018-11-01 18:50:56 -0500

    sequencing

--- assets/test.js
+++ assets/test.js
@@ -1,12 +1,8 @@
-document.getElementById("choice1").innerHTML = "Hello | Goodbye";
-document.getElementById("choice2").innerHTML = "Greetings | Farewell";
-console.log(generateSequence());
-
 var MScards = document.getElementById("MSlist").innerHTML.toString("utf-8").split("\r\n");
-// var MEcards = document.getElementById("MElist").innerHTML.toString("utf-8").split("\r\n");
-// var MTcards = document.getElementById("MTlist").innerHTML.toString("utf-8").split("\r\n");
-// var SEcards = document.getElementById("SElist").innerHTML.toString("utf-8").split("\r\n");
-// var TEcards = document.getElementById("TElist").innerHTML.toString("utf-8").split("\r\n");
+var MEcards = document.getElementById("MElist").innerHTML.toString("utf-8").split("\r\n");
+var MTcards = document.getElementById("MTlist").innerHTML.toString("utf-8").split("\r\n");
+var SEcards = document.getElementById("SElist").innerHTML.toString("utf-8").split("\r\n");
+var TEcards = document.getElementById("TElist").innerHTML.toString("utf-8").split("\r\n");
 
 console.log(MScards);
 
@@ -20,20 +16,91 @@ document.getElementById("spellcount").innerHTML = Sdeck.length;
 document.getElementById("trapcount").innerHTML = Tdeck.length;
 document.getElementById("extracount").innerHTML = Edeck.length;
 
-var category;
-category = "MS";
-$("#first").click(function(){
-	var selected = document.getElementById("choice1").innerHTML;
-    addtodeck(selected);
-});
-$("#second").click(function(){
-	var selected = document.getElementById("choice2").innerHTML;
-	// document.getElementById("monsterlist").innerHTML += selected + "<br />";
-	addtodeck(selected);
-});
-function addtodeck(selected){
-	var card1 = selected.split('|')[0].trim();
-	var card2 = selected.split('|')[1].trim();
+generateSequence();
+function generateSequence(){
+	var sequence = [];
+	for (var i = 0; i < 5; i++){
+		MStoSequence(sequence);
+		MEtoSequence(sequence);
+		MTtoSequence(sequence);
+		SEtoSequence(sequence);
+		TEtoSequence(sequence);
+	}
+	shuffleSequence(sequence);
+	sequence.unshift("foundation");
+	console.log(sequence);
+	runSequence(sequence);
+}
+function MStoSequence(sequence){
+	sequence.push("MS");
+}
+function MEtoSequence(sequence){
+	sequence.push("ME");
+}
+function MTtoSequence(sequence){
+	sequence.push("MT");
+}
+function SEtoSequence(sequence){
+	sequence.push("SE");
+}
+function TEtoSequence(sequence){
+	sequence.push("TE");
+}
+function shuffleSequence(sequence){
+	for (var i = sequence.length -1; i > 0; i--){
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = sequence[i];
+		sequence[i] = sequence[j];
+		sequence[j] = temp;
+	}
+}
+
+function runSequence(sequence){
+	// console.log(sequence);
+	var category = sequence[0].toString();
+	setChoices(sequence, category);
+}
+
+function setChoices(sequence, category){
+	console.log(category);
+	document.getElementById("choice1").innerHTML = "Hello | Goodbye";
+	document.getElementById("choice2").innerHTML = "Greetings | Farewell";
+	$("#first").click(function(){
+		var selected = document.getElementById("choice1").innerHTML;
+    	addtodeck(sequence, selected, category);
+	});
+	$("#second").click(function(){
+		var selected = document.getElementById("choice2").innerHTML;
+		addtodeck(sequence, selected, category);
+	});
+}
+function addtodeck(sequence, selected, category){
+	if (category == "foundation"){
+		console.log(selected);
+		var card1 = selected.split('|')[0].trim();
+		console.log(selected);
+		var card2 = selected.split('|')[1].trim();
+		// var card3 = selected.split('|')[2].trim();
+		// var card4 = selected.split('|')[3].trim();
+		// var card5 = selected.split('|')[4].trim();
+	}
+	else
+	{	
+		var card1 = selected.split('|')[0].trim();
+		var card2 = selected.split('|')[1].trim();
+	}
+	if (category =="foundation"){
+		Mdeck.push(card1);
+		Mdeck.push(card2);
+		// Mdeck.push(card3);
+		// Mdeck.push(card4);
+		// Mdeck.push(card5);
+		document.getElementById("monsterlist").innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + card1 + "<br />";
+		document.getElementById("monsterlist").innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + card2 + "<br />";
+		// document.getElementById("monsterlist").innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + card3 + "<br />";
+		// document.getElementById("monsterlist").innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + card4 + "<br />";
+		// document.getElementById("monsterlist").innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + card5 + "<br />";
+	}
 	if (category =="MS"){
 		Mdeck.push(card1);
 		Sdeck.push(card2);
@@ -68,46 +135,7 @@ function addtodeck(selected){
 	document.getElementById("spellcount").innerHTML = (Sdeck.length);
 	document.getElementById("trapcount").innerHTML = (Tdeck.length);
 	document.getElementById("extracount").innerHTML = (Edeck.length);
+	sequence.shift();
+	runSequence(sequence);
+}
 
-	}
-
-
-function generateSequence(){
-	var sequence = [];
-	for (var i = 0; i < 5; i++){
-		MStoSequence(sequence);
-		MEtoSequence(sequence);
-		MTtoSequence(sequence);
-		SEtoSequence(sequence);
-		TEtoSequence(sequence);
-	}
-	shuffleSequence(sequence);
-	sequence.unshift("foundation");
-	return sequence;
-}
-function MStoSequence(sequence){
-	sequence.push("MS");
-}
-function MEtoSequence(sequence){
-	sequence.push("ME");
-}
-function MTtoSequence(sequence){
-	sequence.push("MT");
-}
-function SEtoSequence(sequence){
-	sequence.push("SE");
-}
-function TEtoSequence(sequence){
-	sequence.push("TE");
-}
-function shuffleSequence(sequence){
-	for (var i = sequence.length -1; i > 0; i--){
-		var j = Math.floor(Math.random() * (i + 1));
-		var temp = sequence[i];
-		sequence[i] = sequence[j];
-		sequence[j] = temp;
-	}
-}
-function runSequence(){
-	
-}
